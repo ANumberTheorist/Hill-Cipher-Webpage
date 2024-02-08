56199e8d258d7619e0b5a7e470f17fae26a22f43
Math Nerd <34165643+MathNerdGamer@users.noreply.github.com>
2024-02-08 15:40:33 -0600

    Update file_handler.h
    Updated to match GitLab version.

--- file_handler.h
+++ file_handler.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef MATH_NERD_HILL_CIPHER_FILE_HANDLER_H
-#define MATH_NERD_HILL_CIPHER_FILE_HANDLER_H
+#ifndef MATH_NERD_FILE_HANDLER__H
+#define MATH_NERD_FILE_HANDLER__H
 #include <algorithm>
 #include <cstdlib>
 #include <cstring>
@@ -14,7 +14,7 @@
 #include <shellapi.h>
 #endif
 
-// The next two functions come from Konrad Rudolph on StackOverflow:
+// The next two functions are modified from Konrad Rudolph's code on StackOverflow:
 // https://stackoverflow.com/a/444614
 template <typename T = std::mt19937>
 auto random_generator() -> T
@@ -28,7 +28,7 @@ auto random_generator() -> T
     return T{ seed_seq };
 }
 
-auto gen_string(std::size_t len = 16) -> std::string
+auto gen_string(std::size_t len = 8) -> std::string
 {
     static constexpr auto chars =
         "0123456789"
@@ -38,73 +38,121 @@ auto gen_string(std::size_t len = 16) -> std::string
     auto dist = std::uniform_int_distribution{ {}, std::strlen(chars) - 1 };
     auto result = std::string(len, '\0');
     std::generate_n(begin(result), len, [&]() { return chars[dist(rng)]; });
+
     return result;
 }
 
 class file_handler
 {
-private:
-    std::string file_name;
+    private:
+        std::string directory_;
+        std::string html_name_;
+        std::string css_name_;
+        std::string js_name_;
 
-public:
-    file_handler() : file_name{ gen_string() }
-    {
-        int len = 16;
-        while( std::filesystem::exists(file_name) )
+        auto strip_exe_name(std::string path) -> std::string
         {
-            file_name = gen_string(len++);
-
-            if( len > 20 )
-            {
-                len = 16;
-            }
+            #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(_WIN64)
+                path.erase(path.find_last_of('\\') + 1);
+            #elif __linux__ || __APPLE__ || __APPLE_CC__ || __unix__
+                path.erase(path.find_last_of('/') + 1);
+            #endif
+            return path;
         }
 
-        file_name += ".html";
+        auto replace_string(std::string &str, std::string const &from, std::string const &to) -> void
         {
-            std::ofstream ofile(file_name);
+            auto start = str.find(from);
+            str.replace(start, from.length(), to);
+        }
 
-            std::string file_content
+        auto file_name(std::string const &directory, char const *ext) -> std::string
+        {
+            std::string name;
+            do
             {
-                #include "hill_cipher.txt"
-            };
+                name = gen_string() + ext;
+            } while( std::filesystem::exists(directory + name) );
 
-            if( std::filesystem::exists("favicon.ico") )
+            return name;
+        }
+
+        auto make_file(std::string const &name, std::string const &content) -> void
+        {
+            std::ofstream out(name);
+            out << content;
+        }
+
+    public:
+        file_handler(char const *directory) :
+            directory_{ strip_exe_name(directory) },
+            html_name_{ file_name(directory_, ".html") },
+            css_name_{ file_name(directory_, ".css") },
+            js_name_{ file_name(directory_, ".js") }
+        {
             {
-                auto head = file_content.find("<head>\n");
-
-                if( head != std::string::npos )
+                // Start HTML file. 
+                std::string html_content
                 {
-                    file_content.insert(head + 7, "\n");
-                    file_content.insert(head + 7, R"html(        <link rel="shortcut icon" href="favicon.ico">)html");
-                }
+                    #include "html.txt"
+                };
+
+                // Start CSS file.
+                std::string css_content
+                {
+                    #include "css.txt"
+                };
+
+                // Put CSS file name into HTML content.
+                replace_string(html_content, "$css_name$", css_name_);
+
+                // Create CSS file.
+                css_name_ = directory_ + css_name_;
+                make_file(css_name_, css_content);
+
+                // Start JS file.
+                std::string js_content
+                {
+                    #include "js.txt"
+                };
+
+                // Put JS file name into HTML content.
+                replace_string(html_content, "$js_name$", js_name_);
+
+                // Create JS file.
+                js_name_ = directory_ + js_name_;
+                make_file(js_name_, js_content);
+
+                // Create HTML file.
+                html_name_ = directory_ + html_name_;
+                make_file(html_name_, html_content);
             }
 
-            ofile << file_content;
-        }
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(_WIN64)
-        ShellExecuteA(0, 0, file_name.c_str(), 0, 0, SW_SHOW);
-#elif __linux__
-        std::string cmd{ "xdg-open ./" + file_name };
+            #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(_WIN64)
+                ShellExecuteA(0, 0, html_name_.c_str(), 0, 0, SW_SHOW);
+            #elif __linux__
+                std::string cmd{ "xdg-open ./" + html_name_ };
 
-        if( std::system(cmd.c_str()) != 0 )
+                if( std::system(cmd.c_str()) != 0 )
+                {
+                    std::cout << "Unable to open browser using xdg-open command.\n";
+                }
+            #elif  __APPLE__ || __APPLE_CC__ || __unix__
+                std::string cmd{ "open ./" + html_name_ };
+
+                if( std::system(cmd.c_str()) != 0 )
+                {
+                    std::cout << "Unable to open browser using open command.\n";
+                }
+            #endif
+        }
+
+
+        ~file_handler()
         {
-            std::cout << "Unable to open browser using xdg-open command.\n";
+            std::filesystem::remove(html_name_);
+            std::filesystem::remove(css_name_);
+            std::filesystem::remove(js_name_);
         }
-#elif  __APPLE__ || __APPLE_CC__ || __unix__
-        std::string cmd{ "open ./" + file_name };
-
-        if( std::system(cmd.c_str()) != 0 )
-        {
-            std::cout << "Unable to open browser using open command.\n";
-        }
-#endif
-    }
-
-
-    ~file_handler()
-    {
-        std::filesystem::remove(file_name);
-    }
 };
-#endif
+#endif // MATH_NERD_FILE_HANDLER__H
